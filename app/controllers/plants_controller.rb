62e6acb6fec916348fc0d558057a6dad18a12649
iraholmes <ira.holmes63@gmail.com>
2023-04-03 16:21:16 -0700

    API-EndPoints completed

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,45 @@
 class PlantsController < ApplicationController
+    def index
+    plants = Plant.all
+    render json: plants
+    end
+
+    def create
+    plant = Plant.create(plant_params)
+    render json: plant
+    end
+
+    def update
+        plant = Plant.find(params[:id])
+        plant.update(plant_params)
+        if plant.valid?
+            render json: plant
+        end
+    end
+
+    def destroy
+        plant = Plant.find(params[:id])
+        plant.destroy
+        render json: plant
+    end
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private
+    def plant_params
+    params.require(:plant).permit(:name, :age, :enjoy, :image)
+    end
 end
+
