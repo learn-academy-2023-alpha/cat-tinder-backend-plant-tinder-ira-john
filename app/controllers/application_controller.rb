df8d973bbf8dff1ffb404f576ce6af1bb4b5cdc3
iraholmes <ira.holmes63@gmail.com>
2023-04-03 14:50:00 -0700

    Backend-Structure completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+skip_for_action :verify_authenticity_token
 end
---
+++ db/migrate/20230403185027_create_plants.rb
@@ -0,0 +1,12 @@
+class CreatePlants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :plants do |t|
+      t.string :name
+      t.integer :age
+      t.text :enjoy
+      t.text :image
+
+      t.timestamps
+    end
+  end
+end
